f5bf7afb2fd0db702c2205aece06e225643321c9
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-04-10 21:39:27 +0300

    enable rest service calls threw swagger

--- src/main/java/com/asen/kafka/security/SecurityConfig.java
+++ src/main/java/com/asen/kafka/security/SecurityConfig.java
@@ -10,8 +10,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-     security.httpBasic().disable();
+    protected void configure(HttpSecurity http) throws Exception
+    {     
+     http
+		.csrf().disable()
+		.authorizeRequests().antMatchers("/**").permitAll()
+		.and()
+		.httpBasic().disable();
     }
 }
