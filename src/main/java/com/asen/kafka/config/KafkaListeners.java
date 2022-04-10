8c33cccdfcb403e1e4d85c2e29730a1d41e3fec8
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-04-10 22:17:59 +0300

    implement kafka listener and recieve message

---
+++ src/main/java/com/asen/kafka/config/KafkaListeners.java
@@ -0,0 +1,14 @@
+package com.asen.kafka.config;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+	
+	@KafkaListener(topics = "AsenTopic", groupId = "initGroupId")
+	public void listenGroupInitGroupId(String message) {
+		System.out.println("Received Message in group id initGroupId: " + message);
+	}
+
+}
